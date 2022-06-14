9e3c6c4f45b85e6d748e7b63c5b22da699b4d589
Ren <yirenlu92@users.noreply.github.com>
2022-06-14 15:50:44 -0400

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,23 +1,19 @@
-import { StaticCeloProvider } from '@celo-tools/celo-ethers-wrapper';
+import { StaticCeloProvider } from '@celo-tools/celo-ethers-wrapper'; // TODO: Update this to an SDK for your source chain
 import { providers } from 'ethers';
 
 export async function fetchBlock(
   endpoint: string,
   height: number
 ): Promise<any> {
+  
+  // TODO: Update line below to connect to appropriate SDK
   const provider = new StaticCeloProvider(endpoint);
+  // TODO: Update line below to get data from the appropriate RPC endpoint
   const block = await provider.getBlockWithTransactions(height);
 
   // The block is always defined, unless the height is out of range.
   if (block) {
-    // Delete the number of confirmations from a transaction to maintain determinism.
-    block.transactions.forEach(
-      (tx: Partial<providers.TransactionResponse>) => delete tx.confirmations
-    );
-
-    // TODO: Figure out why `extraData` varies for some blocks.
-    // @ts-ignore
-    delete block.extraData;
+   // TODO: Implement post-processing of the data that comes back
   }
 
   return block;
